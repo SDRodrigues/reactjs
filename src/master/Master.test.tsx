de1b64ff043ea3f67f7985bbae8c751b88332877
sdrodrigues <infantariamilitar@gmail.com>
2020-06-16 18:05:19 -0300

    test jest

---
+++ src/master/Master.test.tsx
@@ -0,0 +1,9 @@
+import Master from "./Master";
+import React from "react";
+import {render} from "react-dom";
+
+describe('Test for MasterComponent', () => {
+    it('should return PrimeiroComponent ',  () => {
+
+    });
+})
---
+++ src/master/master-filho/Filho.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {render} from "react-dom";
+import Filho from "./Filho";
+
+describe('Test for FilhoComponent', () => {
+    it('should return PrimeiroComponent ',  () => {
+
+    });
+})
